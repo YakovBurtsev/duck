595116bb17960846b5ae521d82bd307d32becf58
yburtsev <yakovburtsev@gmail.com>
2019-06-06 20:53:21 +0300

    switch

--- app/src/main/java/com/yaburtsev/duck/MainActivity.java
+++ app/src/main/java/com/yaburtsev/duck/MainActivity.java
@@ -1,8 +1,9 @@
 package com.yaburtsev.duck;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Switch;
 import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,14 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        ToggleButton toggleButton = findViewById(R.id.toggleButton);
+    public void onToggleButtonClicked(View view) {
+        ToggleButton toggleButton = (ToggleButton) view;
+
         if (toggleButton.isChecked()) {
             //on
         } else {
             //off
         }
+    }
 
+    public void onSwitchClicked(View view) {
+        Switch switchView = (Switch) view;
 
+        if (switchView.isChecked()) {
+            //on
+        } else {
+            //off
+        }
     }
 }
