d8a2b5e25e7ac9e7591d017b1997d3a6df4b5a58
yburtsev <yakovburtsev@gmail.com>
2019-06-06 20:48:53 +0300

    toggle button

--- app/src/main/java/com/yaburtsev/duck/MainActivity.java
+++ app/src/main/java/com/yaburtsev/duck/MainActivity.java
@@ -2,6 +2,8 @@ package com.yaburtsev.duck;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ToggleButton toggleButton = findViewById(R.id.toggleButton);
+        if (toggleButton.isChecked()) {
+            //on
+        } else {
+            //off
+        }
+
+
     }
 }
